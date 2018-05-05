d8c2defdc1993dd4075d3cb1fb8040ca2345645c
Kenneth Humphries <humphrik@bxscience.edu>
2018-05-04 23:46:59 -0400

    2P game functional

--- Assets/CheckerPiece.cs
+++ Assets/CheckerPiece.cs
@@ -1,8 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class CheckerPiece {
+public class CheckerPiece : MonoBehaviour{
+
+	public Transform highlight = null;
+
 	public int state = (int)BoardStates.States.None;
-	public float[] coords = new float[2];
+	public int[] coords = new int[2];
+	public static Vector3[] diagonals = new Vector3[] {
+		Vector3.forward + Vector3.left,
+		Vector3.forward + Vector3.right,
+		Vector3.back + Vector3.right,
+		Vector3.back + Vector3.left };
+	public static int[,] diagonalCoords = new int[,] {{-1,1}, {1,1}, {1,-1}, {-1,-1}};
+
+
+	public void CheckValidMoves() {
+		// Debug.Log ("(" + coords[0] + "," + coords[1] + ")");
+		for (int i = 0; i <= 3; i++) {
+			if((state == (int)BoardStates.States.Red && i >= 2) 
+			   || (state == (int)BoardStates.States.Blue && i < 2))
+				continue;
+
+			RaycastHit hit, hit2;
+			if (Physics.Raycast (transform.position, diagonals[i] , out hit, 32)) {
+				int hitState = hit.transform.GetComponent<CheckerPiece> ().state;
+				if ((state == 1 || state == 3) && (hitState == 2 || hitState == 4)
+				    || (state == 2 || state == 4) && (hitState == 1 || hitState == 3)) {
+					// ***^It's hard to keep using the enum, okay?^***
+					if (!Physics.Raycast (hit.transform.position, diagonals [i], out hit2, 32)) {
+						int newX = coords[0] + 2 * diagonalCoords[i,0];
+						int newY = coords[1] + 2 * diagonalCoords[i,1];
+
+						if (IsValidPosition (newX, newY)) {
+							// A valid capture!
+							// Debug.Log ("Valid Space at: " + "(" + newX + ", " + newY + ")");
+							float[] worldCoords = CoordToWorldPosition (newX, newY);
+							Transform potentialMove = GameObject.Instantiate (highlight, new Vector3 (worldCoords [0], 0.1f, worldCoords [1]), highlight.rotation);
+							Highlight highlightData = potentialMove.GetComponent<Highlight> ();
+							highlightData.checkerPiece = transform;
+							highlightData.capturePiece = hit.transform;
+							highlightData.coords = new int[] {newX, newY};
+								
+						}
+					}
+
+				}
+
+
+			}
+			else {
+				int newX = coords[0] + diagonalCoords[i,0];
+				int newY = coords[1] + diagonalCoords[i,1];
+
+				if (IsValidPosition (newX, newY)) {
+					// A conventional move.
+					// Debug.Log ("Valid Space at: " + "(" + newX + ", " + newY + ")");
+					float[] worldCoords = CoordToWorldPosition (newX, newY);
+					Transform potentialMove = GameObject.Instantiate (highlight, new Vector3 (worldCoords [0], 0.1f, worldCoords [1]), highlight.rotation);
+					Highlight highlightData = potentialMove.GetComponent<Highlight> ();
+					highlightData.checkerPiece = transform;
+					highlightData.coords = new int[] {newX, newY};
+				}
+			}
+		}
+	}
+
+	public bool IsValidPosition(int x, int y) {
+		return x >= 0 && x <= 7 && y >= 0 && y <= 7;
+	}
+
+	public float[] CoordToWorldPosition(int x, int y) {
+		float xPos = 20 * x - 70;
+		float yPos = 20 * y - 70;
+		return new float[] {xPos, yPos};
+	}
+
+	public int[] WorldToCoordPosition(float x, float y) {
+		float xPos = (x + 70)/20;
+		float yPos = (y + 70)/20;
+		return new int[] {(int)(xPos + 0.5f), (int)(yPos + 0.5f)};
+	}
 }
--- Assets/InitializeBoard.cs
+++ Assets/InitializeBoard.cs
@@ -6,6 +6,8 @@ public class InitializeBoard : MonoBehaviour {
 
 	const int BOARD_SIZE = 8;
 	int[,] gridStates = new int[8, 8];
+	int playerTurn = 1;
+	// Player Turns alternate between 1 (red) and -1 (blue).
 
 	public Transform redChecker, blueChecker = null;
 
@@ -19,7 +21,7 @@ public class InitializeBoard : MonoBehaviour {
 				Transform checker = GameObject.Instantiate(redChecker, new Vector3(coords[0], 0f, coords[1]),
 				                                            redChecker.rotation);
 				checker.GetComponent<CheckerPiece>().state = (int)BoardStates.States.Red;
-				checker.GetComponent<CheckerPiece>().coords = new float[] {xAdjusted, i};
+				checker.GetComponent<CheckerPiece>().coords = new int[] {xAdjusted, i};
 				gridStates[xAdjusted,j] = (int)BoardStates.States.Red;
 			}
 		}
@@ -31,15 +33,35 @@ public class InitializeBoard : MonoBehaviour {
 				Transform checker = GameObject.Instantiate(blueChecker, new Vector3(coords[0], 0f, coords[1]),
 				                       blueChecker.rotation);
 				checker.GetComponent<CheckerPiece>().state = (int)BoardStates.States.Blue;
-				checker.GetComponent<CheckerPiece>().coords = new float[] {xAdjusted, i};
+				checker.GetComponent<CheckerPiece>().coords = new int[] {xAdjusted, i};
 				gridStates[xAdjusted,j] = (int)BoardStates.States.Blue;
 			}
 		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update(){
+		if (Input.GetMouseButtonDown(0)){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)){
+				if (hit.transform.gameObject.CompareTag ("GamePiece")) {
+					int hitState = hit.transform.GetComponent<CheckerPiece> ().state;
+					// Wrong turn, dummy.
+					if ((hitState == 1 || hitState == 3) && playerTurn == -1
+					    || (hitState == 2 || hitState == 4) && playerTurn == 1)
+						return;
+					foreach (GameObject highlights in GameObject.FindGameObjectsWithTag("Highlight"))
+						GameObject.Destroy (highlights);
+					hit.transform.gameObject.GetComponent<CheckerPiece> ().CheckValidMoves ();
+				} else if (hit.transform.gameObject.CompareTag ("Highlight")) {
+					hit.transform.GetComponent<Highlight> ().ConfirmMove ();
+					foreach (GameObject highlights in GameObject.FindGameObjectsWithTag("Highlight"))
+						GameObject.Destroy (highlights);
+					playerTurn *= -1;
+				}
+			}
+		}
 	}
 
 	float[] CoordToWorldPosition(int x, int y) {
