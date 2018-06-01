8058a3fc61d9b131dec7ba527b4710f8bcabcb6e
Quinn <edwardsq@bxscience.edu>
2018-06-01 13:30:53 -0400

    I'm bad at writing comit summaries

--- Assets/Scripts/MinimaxBase.cs
+++ Assets/Scripts/MinimaxBase.cs
@@ -14,36 +14,37 @@ public class MinimaxBase : MonoBehaviour {
 	private int maxDepth = 1;
 
 	public GameObject manager;
+
+	public bool minimaxRunning = false;
 	
 	// If it's the correct turn and the AI is active, minimax is run
 	void Update () {
-		if (minimaxActive && manager.GetComponent<InitializeBoard>().playerTurn == 1) {
+		if (minimaxActive && manager.GetComponent<InitializeBoard>().playerTurn == -1&&minimaxRunning==false) {
+			minimaxRunning = true;
 			minimax();
 		}
 	}
 
 	//Easier way of referring to the corners.
-	int[][] Corners = new int[4][];
-//	Corners [0] = new int [] {1,1,0};
-//	Corners [1] = new int [] {1,-1,0};
-//	Corners [2] = new int [] {-1,1,0};
-//	Corners [3] = new int [] {-1,-1,0};
+	public static int[,] diagonalCoords = new int[,] {{-1,1}, {-1,-1}, {1,-1}, {1,1}};
 
 	void minimax (){
-		Move bestMove;
+		Move bestMove = new Move();
 		int bestScore = -9999;
 		boardToArray();
 		List<Move> legal = getLegalMoves(2);
 		foreach (Move move in legal) {
-			//make (move);
+			make (move);
 			int score = min (0);
 			if (score > bestScore) {
 				bestScore = score;
 				bestMove = move;
 			}
-			//retract (move);
+			retract (move);
 		}
-
+		MakeMove (bestMove);
+		manager.GetComponent<InitializeBoard> ().playerTurn = 1;
+		minimaxRunning = false;
 	}
 
 	//Finds best move for AI
@@ -56,12 +57,12 @@ public class MinimaxBase : MonoBehaviour {
 			int bestScore = 9999;
 			List<Move> legal = getLegalMoves(2);
 			foreach (Move move in legal){
-				//make (move);
+				make (move);
 				int score = max (depth+1);
 				if (score<bestScore){
 					bestScore=score;
 				}
-				//retract (move);
+				retract (move);
 			}
 			return bestScore;
 		}
@@ -77,12 +78,12 @@ public class MinimaxBase : MonoBehaviour {
 			int bestScore = -9999;
 			List<Move> legal = getLegalMoves(1);
 			foreach (Move move in legal){
-				//make (move);
+				make (move);
 				int score = min (depth+1);
 				if (score>bestScore){
 					bestScore=score;
 				}
-				//retract (move);
+				retract (move);
 			}
 			return bestScore;
 		}
@@ -97,6 +98,9 @@ public class MinimaxBase : MonoBehaviour {
 		}
 		foreach (GameObject checker in GameObject.FindGameObjectsWithTag("GamePiece")) {
 			board [checker.GetComponent<CheckerPiece>().coords [0],checker.GetComponent<CheckerPiece>().coords [1]] = checker.GetComponent<CheckerPiece>().state;
+		}
+		foreach (int con in board) {
+			Debug.Log(con.ToString());
 		}
 	}
 
@@ -126,12 +130,26 @@ public class MinimaxBase : MonoBehaviour {
 	//Gets list of moves that can be made adjecently
 	List<Move> getAdjacentMoves(int[] coords){
 		List<Move> moves = new List<Move> ();
-		if (board [coords [0] + 1, coords [1] + 1] == 0 || board [coords [0] - 1, coords [1] + 1] == 0) {
-			//Add the move
-		}
-		if (coords [2] == 3 || coords [2] == 4) {
-			if (board [coords [0] + 1, coords [1] - 1] == 0 || board [coords [0] - 1, coords [1] - 1] == 0) {
-				//Add the move
+		if (coords [2] != 4) {
+			for (int i = 0; i < 2; i++) {
+				if (IsValidPosition(coords [0] + diagonalCoords [i, 0], coords [1] + diagonalCoords [i, 1])){	
+					if (board [coords [0] + diagonalCoords [i, 0], coords [1] + diagonalCoords [i, 1]]==0) {
+						int[] newEnd = new int[]{ coords [0] + diagonalCoords [i, 0], coords [1] + diagonalCoords [i, 1], coords [2] };
+						Move adjacent = new Move (coords, newEnd);
+						moves.Add (adjacent);
+					}
+				}
+			}
+		} 
+		else {
+			for (int i = 0; i < 4; i++) {
+				if (IsValidPosition (coords [0] + diagonalCoords [i, 0], coords [1] + diagonalCoords [i, 1])) {
+					if (board [coords [0] + diagonalCoords [i, 0], coords [1] + diagonalCoords [i, 1]] == 0) {
+						int[] newEnd = new int[]{ coords [0] + diagonalCoords [i, 0], coords [1] + diagonalCoords [i, 1], coords [2] };
+						Move adjacent = new Move (coords, newEnd);
+						moves.Add (adjacent);
+					}
+				}
 			}
 		}
 		return moves;
@@ -139,30 +157,44 @@ public class MinimaxBase : MonoBehaviour {
 
 	//Gets list of captures that can be made
 	List<Move> getCaptures(int[] coords){
-		int color = coords [2];
+		int color = coords [2]%2;
+		int oppo = (coords [2] + 1) % 2;
 		List<Move> moves = new List<Move> ();
-		if (board [coords [0] + 1, coords [1] - 1] == 0 || board [coords [0] - 1, coords [1] - 1] == 0) {
-			
-		} 
+		for (int i=0; i<4; i++) {
+			if(IsValidPosition(coords[0]+diagonalCoords[i,0], coords[1]+diagonalCoords[i,1])){
+				if(board[coords[0]+diagonalCoords[i,0], coords[1]+diagonalCoords[i,1]]%2==oppo){
+					if(board[coords[0]+2*diagonalCoords[i,0], coords[1]+2*diagonalCoords[i,1]]==0){
+						List<int[]> captured = new List<int[]> ();
+						captured.Add (new int[]{ coords [0] + 2 * diagonalCoords [i, 0], coords [1] + 2 * diagonalCoords [i, 1] });
+						Move capturing = new Move(coords, new int[] {coords[0]+2*diagonalCoords[i,0], coords[1]+2*diagonalCoords[i,1], coords[2]}, captured);
+						moves.Add (capturing);
+					}
+				}
+			}
+		}
 		moves.AddRange (getCaptures (moves));
 		return moves;
 	}
 
 	List<Move> getCaptures(List<Move> moves){
 		List<Move> output = new List<Move> ();
-		//FIX THIS
 		int opponent = 0;
 		foreach (Move move in moves) {
-			foreach (int[] corner in Corners) {
-				if (board [move.endPos [0] + corner [0], move.endPos [1] + corner [1]] == opponent && board [move.endPos [0] + corner [0] + corner [0], move.endPos [1] + corner [1] + corner [1]] == 0) {
-					int[] newStart = move.startPos;
-					int[] newEnd = new int[] {move.endPos [0] + 2 * corner [0], move.endPos [1] + 2 * corner [1] };
-					int[] captured = new int[] {move.endPos [0] + corner [0], move.endPos [1] + corner [1] };
-					Move toAdd = new Move (newStart, newEnd, move.capped);
-					toAdd.capped.Add (captured);
-					output.Add (toAdd);
+			for (int i = 0; i<4; i++) {
+				if (IsValidPosition (move.endPos [0] + diagonalCoords [i, 0], move.endPos [1] + diagonalCoords [i, 1]) && IsValidPosition (move.endPos [0] + diagonalCoords [i, 0] + diagonalCoords [i, 0], move.endPos [1] + diagonalCoords [i, 1] + diagonalCoords [i, 1])) {
+					if (board [move.endPos [0] + diagonalCoords [i, 0], move.endPos [1] + diagonalCoords [i, 1]] == opponent && board [move.endPos [0] + diagonalCoords [i, 0] + diagonalCoords [i, 0], move.endPos [1] + diagonalCoords [i, 1] + diagonalCoords [i, 1]] == 0) {
+						int[] newStart = move.startPos;
+						int[] newEnd = new int[] { move.endPos [0] + 2 * diagonalCoords [i, 0], move.endPos [1] + 2 * diagonalCoords [i, 1] };
+						int[] captured = new int[] { move.endPos [0] + diagonalCoords [i, 0], move.endPos [1] + diagonalCoords [i, 1] };
+						Move toAdd = new Move (newStart, newEnd, move.capped);
+						toAdd.capped.Add (captured);
+						output.Add (toAdd);
+					}
 				}
 			}
+		}
+		if (output.Count == 0) {
+			return output;
 		}
 		output.AddRange (getCaptures (output));
 		return output;
@@ -225,5 +257,38 @@ public class MinimaxBase : MonoBehaviour {
 
 	int distToEdge(){
 		return 0;
+	}
+
+	float[] CoordToWorldPosition(int x, int y) {
+		// Translates from an 8x8 grid state to an actual place in the world.
+		// **IMPORTANT: Y-AXIS ON THE GRID IS Z-AXIS IN THE GAME WOLRD.**
+		float xPos = 20 * x - 70;
+		float yPos = 20 * y - 70;
+		return new float[] {xPos, yPos};
+	}
+
+	void MakeMove (Move finalMove){
+		GameObject piece = null;
+		//Finds the active checker
+		foreach (GameObject checker in GameObject.FindGameObjectsWithTag("GamePiece")) {
+			if (checker.GetComponent<CheckerPiece> ().coords[0] == finalMove.startPos [0] && checker.GetComponent<CheckerPiece> ().coords[1] == finalMove.startPos [1])
+				piece = checker;
+		}
+
+		//Moves the checker
+		float [] finalPosition = CoordToWorldPosition (finalMove.endPos[0], finalMove.endPos[0]);
+		piece.transform.position = new Vector3(finalPosition[0], finalPosition[1]);
+
+		//Destroy captured pieces
+		foreach (int[] cappedPiece in finalMove.capped) {
+			foreach (GameObject checker in GameObject.FindGameObjectsWithTag("GamePiece")) {
+				if (checker.GetComponent<CheckerPiece> ().coords[0] == cappedPiece [0] && checker.GetComponent<CheckerPiece> ().coords[1] == cappedPiece [1])
+					GameObject.Destroy (checker);
+			}
+		}
+	}
+
+	public bool IsValidPosition(int x, int y) {
+		return x >= 0 && x <= 7 && y >= 0 && y <= 7;
 	}
 }
